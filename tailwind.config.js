e2940e3043defc2ed83a0a92127d7e2f8d67994a
Daniel Brož <danobroz66@gmail.com>
2022-11-04 21:44:08 +0100

    add base settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+    content: [
+        './src/**/*.{js,jsx,ts,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+        container: {
+            center: true,
+            screens: {
+                '2xl': '1110px',
+            },
+        },
+        extend: {
+            colors: {
+                Black: {
+                    light: '#101010',
+                    dark: '#000000',
+                },
+                White: {
+                    shade: '#fafafa',
+                },
+                Red: {
+                    dark: 'CD2C2C',
+                },
+                Blue: {
+                    light: '#F1F1F1',
+                },
+                Orange: {
+                    light: '#fbaf85',
+                    dark: '#D87D4A',
+                },
+            },
+        },
+    },
+    plugins: [],
+}
