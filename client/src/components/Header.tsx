eb8ebb3ba1effeef5b96472ec9eb2e2fa69cf13e
Daniel Brož <danobroz66@gmail.com>
2022-11-05 19:35:53 +0100

    add header component

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -6,28 +6,30 @@ export const Header = () => {
     return (
         <header className='absolute top-0 left-0 right-0'>
             <div className='container flex justify-between border-b border-white/20 pt-8 pb-9'>
-                <Logo />
-                <div className='flex gap-[2.125rem] text-[0.8125rem] uppercase leading-[1.5625rem] tracking-[0.125rem] text-white'>
+                <Link to='/'>
+                    <Logo />
+                </Link>
+                <div className='flex gap-[2.125rem] text-[0.8125rem] uppercase leading-[1.5625rem] tracking-[0.125rem] text-white lg:mr-[12.5%]'>
                     <Link
-                        className='transition-colors hover:text-white/80'
-                        to='/home'
+                        className='transition-colors hover:text-Orange-dark'
+                        to='/'
                     >
                         home
                     </Link>
                     <Link
-                        className='transition-colors hover:text-white/80'
+                        className='transition-colors hover:text-Orange-dark'
                         to='/headphones'
                     >
                         headphones
                     </Link>
                     <Link
-                        className='transition-colors hover:text-white/80'
+                        className='transition-colors hover:text-Orange-dark'
                         to='/speakers'
                     >
                         speakers
                     </Link>
                     <Link
-                        className='transition-colors hover:text-white/80'
+                        className='transition-colors hover:text-Orange-dark'
                         to='/earphones'
                     >
                         earphone
