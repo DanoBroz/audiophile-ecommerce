20d1b1ea962c016c1a9f88d9be47ad5344471b99
Daniel Brož <danobroz66@gmail.com>
2022-11-06 22:03:53 +0100

    use navlinks component within header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
 import { ReactComponent as Logo } from '../assets/shared/desktop/logo.svg'
 import { ReactComponent as CartIcon } from '../assets/shared/desktop/icon-cart.svg'
+import { NavLinks } from './NavLinks'
 
 export const Header = () => {
     return (
@@ -9,32 +10,7 @@ export const Header = () => {
                 <Link to='/'>
                     <Logo />
                 </Link>
-                <div className='flex gap-[2.125rem] text-[0.8125rem] uppercase leading-[1.5625rem] tracking-[0.125rem] text-white lg:mr-[12.5%]'>
-                    <Link
-                        className='transition-colors hover:text-Orange-dark'
-                        to='/'
-                    >
-                        home
-                    </Link>
-                    <Link
-                        className='transition-colors hover:text-Orange-dark'
-                        to='/headphones'
-                    >
-                        headphones
-                    </Link>
-                    <Link
-                        className='transition-colors hover:text-Orange-dark'
-                        to='/speakers'
-                    >
-                        speakers
-                    </Link>
-                    <Link
-                        className='transition-colors hover:text-Orange-dark'
-                        to='/earphones'
-                    >
-                        earphone
-                    </Link>
-                </div>
+                <NavLinks className='lg:pr-[11%]' />
                 <CartIcon />
             </div>
         </header>
