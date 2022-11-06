830e766988d606de83f297e328564d5b86ae2760
Daniel Brož <danobroz66@gmail.com>
2022-11-06 22:05:01 +0100

    use footer component within page container

--- client/src/containers/PageContainer.tsx
+++ client/src/containers/PageContainer.tsx
@@ -1,12 +1,12 @@
 import { Outlet } from 'react-router-dom'
-import { Header } from '../components'
+import { Footer, Header } from '../components'
 
 export const PageContainer = () => {
     return (
         <div className='relative'>
             <Header />
             <Outlet />
-            <footer>footer here</footer>
+            <Footer />
         </div>
     )
 }
