9844af40e1aca44b64d1639f15b32da8ba43662d
Daniel Brož <danobroz66@gmail.com>
2022-11-07 16:16:50 +0100

    create category header

---
+++ client/src/components/CategoryHeader.tsx
@@ -0,0 +1,20 @@
+import type { HTMLAttributes } from 'react'
+
+interface HeaderConfig {
+    headingText: string
+}
+
+type CategoryHeaderProps = HeaderConfig & HTMLAttributes<HTMLElement>
+
+export const CategoryHeader = (props: CategoryHeaderProps) => {
+    const { className, headingText, ...elementProps } = props
+
+    return (
+        <section
+            className={`mb-40 rounded-b-lg bg-black bg-contain bg-center bg-no-repeat pt-[12.1875rem] text-center ${className}`}
+            {...elementProps}
+        >
+            <h2 className='pb-[97px]'>{headingText}</h2>
+        </section>
+    )
+}
