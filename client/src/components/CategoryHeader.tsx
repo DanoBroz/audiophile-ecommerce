a14051722ec6296e978b736e48d819111a0c54cc
Daniel Brož <danobroz66@gmail.com>
2022-11-07 16:19:45 +0100

    change category title color

--- client/src/components/CategoryHeader.tsx
+++ client/src/components/CategoryHeader.tsx
@@ -11,7 +11,7 @@ export const CategoryHeader = (props: CategoryHeaderProps) => {
 
     return (
         <section
-            className={`mb-40 rounded-b-lg bg-black bg-contain bg-center bg-no-repeat pt-[12.1875rem] text-center ${className}`}
+            className={`mb-40 rounded-b-lg bg-black bg-contain bg-center bg-no-repeat pt-[12.1875rem] text-center text-white ${className}`}
             {...elementProps}
         >
             <h2 className='pb-[97px]'>{headingText}</h2>
