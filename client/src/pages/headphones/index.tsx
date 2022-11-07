e408a70c49d8f6032823602eae877236017abf32
Daniel Brož <danobroz66@gmail.com>
2022-11-07 16:20:09 +0100

    add headphones setup to router

--- client/src/pages/headphones/index.tsx
+++ client/src/pages/headphones/index.tsx
@@ -1,5 +1,9 @@
+import { CategoryHeader } from '../../components'
+
 export const Headphones = () => {
     return (
-        <section className='mb-40 rounded-b-lg bg-black bg-contain bg-center bg-no-repeat pt-[12.1875rem]'></section>
+        <>
+            <CategoryHeader headingText='headphones' />
+        </>
     )
 }
