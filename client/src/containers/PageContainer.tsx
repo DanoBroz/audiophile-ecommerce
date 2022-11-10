c1d1a5d9e6db37b0f8fa240c4b9815c85472ecb4
Daniel Brož <danobroz66@gmail.com>
2022-11-10 19:09:21 +0100

    render pages on top

--- client/src/containers/PageContainer.tsx
+++ client/src/containers/PageContainer.tsx
@@ -1,7 +1,14 @@
-import { Outlet } from 'react-router-dom'
+import { useLayoutEffect } from 'react'
+import { Outlet, useLocation } from 'react-router-dom'
 import { Footer, Header } from '../components'
 
 export const PageContainer = () => {
+    const location = useLocation()
+
+    useLayoutEffect(() => {
+        document.documentElement.scrollTo(0, 0)
+    }, [location.pathname])
+
     return (
         <div className='relative'>
             <Header />
